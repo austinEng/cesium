ee572151027f743982238ae20f70eb127e0894d3
Austin Eng <213reeses@gmail.com>
2017-01-30 09:47:23 -0500

    add show frustum planes to inspector

--- Source/Scene/FrameState.js
+++ Source/Scene/FrameState.js
@@ -249,6 +249,18 @@ define([
              */
             outOfView : true
         };
+
+        /**
+        * The position of the splitter to use when rendering imagery layers on either side of a splitter.
+        * This value should be between 0.0 and 1.0 with 0 being the far left of the viewport and 1 being the far right of the viewport.
+        * @type {Number}
+        * @default 0.0
+        */
+        this.imagerySplitPosition = 0.0;
+
+        this.near = 1.0;
+        this.far = 1000.0;
+        this.farToNearRatio = 1000.0;
     }
 
     FrameState.prototype.addCommand = function(command) {
